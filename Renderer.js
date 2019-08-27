f2ed5c61e5c7182d73ae73fc930b579b050363a5
Leah Chercham <leahchercham@hotmail.com>
2019-08-27 10:29:34 -0500

    basics

--- Renderer.js
+++ Renderer.js
@@ -2,27 +2,46 @@
 
 class Renderer {
     _renderUsers(users) {
-
+        const source = $(".user-template").html()
+        const template = Handlebars.compile(source)
+        const newHtml = template({})
+        $(".").append(newHtml)
     }
 
     _renderFriends(users) {
-
+        const source = $(".friends-template").html()
+        const template = Handlebars.compile(source)
+        const newHtml = template({})
+        $(".").append(newHtml)
     }
 
-    _renderQuote(quoteInfo) {
 
-    }
+_renderQuote(quoteInfo) {
+    const source = $(".quote-template").html()
+    const template = Handlebars.compile(source)
+    const newHtml = template({})
+    $(".").append(newHtml)
+}
 
-    _renderPokemon(pokemonInfo) {
 
-    }
+_renderPokemon(pokemonInfo) {
+    const source = $(".pokemon-template").html()
+    const template = Handlebars.compile(source)
+    const newHtml = template({})
+    $(".").append(newHtml)
+}
+    
 
-    _renderMeat(meatText) {
+_renderMeat(meatText) {
+    const source = $(".meat-template").html()
+    const template = Handlebars.compile(source)
+    const newHtml = template({})
+    $(".").append(newHtml)
+}
+    
 
-    }
-
-    render(data){
-        //invokes all the individual _render methods
-    }
+render(data){
+    //invokes all the individual _render methods
+}
 }
 
